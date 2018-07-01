f0f5eed2eb5bc7ba26240aa7d502d7e28fa2eaec
Naesstrom <naesstrom@gmail.com>
2018-07-01 23:51:49 +0200

    Updated files

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,9 @@ FROM $BUILD_FROM
 
 ENV LANG C.UTF-8
 
+# Install requirements for add-on
+RUN apk add --no-cache jq
+
 # Copy data for add-on
 COPY run.sh /
 
--- run.sh
+++ run.sh
@@ -1,1 +1,6 @@
-python3 -m http.server
+docker build -t sparsnas --build-arg SENSORS="1234 1000" \
+  --build-arg MQTT_HOST=192.168.x.x --build-arg MQTT_PORT=1883 \
+  --build-arg MQTT_USERNAME=username --build-arg MQTT_PASSWORD=password \
+  https://github.com/tubalainen/sparsnas_decoder.git
+
+docker run -it --device=/dev/bus/usb --name=sparsnas --restart=always sparsnas:latest
